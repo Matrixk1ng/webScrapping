ef546cdf345a9f4c91135fd1b8fdf62ca653e88c
Obinna Anaenugwu <oeanaenugwu@gmail.com>
2023-12-15 22:59:11 -0600

    shesh

--- scrapping.py
+++ scrapping.py
@@ -1,10 +1,5 @@
 from bs4 import BeautifulSoup
-import requests
 
-url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue'
+with open("index.html", "r") as f:
+    
 
-page = requests.get(url)
-
-soup = BeautifulSoup(page.text, 'html')
-
-print(soup)
